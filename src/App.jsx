4ae8213a0ff26cdc9d8f692f61bbf0f27ccc70b1
vikas-gupta9 <118959552+vikas-gupta9@users.noreply.github.com>
2024-02-01 14:35:51 +0530

    Changed the name todo to product

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom";
-import Todo from "./components/todo/todo";
 import { useCookies } from "react-cookie";
 import {Provider} from "react-redux"
 import store from "./redux/store/store";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
 
@@ -15,8 +15,8 @@ function App() {
     <>
     <Provider store={store}>
       <Routes>
-        <Route exact path="/" element={loggedIn === true ? <Todo/> : <Login />} />
-        <Route exact path="/dashboard" element={<Todo/>} />
+        <Route exact path="/" element={loggedIn === true ? <Dashboard/> : <Login />} />
+        <Route exact path="/dashboard" element={<Dashboard/>} />
       </Routes>
       </Provider>
     </>
